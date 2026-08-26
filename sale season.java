b0e287a0e042e655e5da723a54b09adf75da0b9b
Shruti <shruti.developer18@gmail.com>
2026-08-26 19:02:05 +0530

    Create sale season.java

---
+++ sale season.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for(int i=0;i<t;i++){
+		    int x=s.nextInt();
+		    if(x<=100){
+		        System.out.println(x);
+		    }
+		    else if(x>100&&x<=1000){
+		        System.out.println(x-25);
+		    }
+		    else if(x>1000&&x<=5000){
+		        System.out.println(x-100);
+		    }
+		    else{
+		        System.out.println(x-500);
+		    }
+		}
+
+	}
+}

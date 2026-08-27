454fc9a2e161fc91b5365f832dc1d898fda34f4d
Shruti <shruti.developer18@gmail.com>
2026-08-27 22:36:42 +0530

    Create chefland games.java

---
+++ chefland games.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for(int i=0;i<t;i++){
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    int c=s.nextInt();
+		    int d=s.nextInt();
+		    int sum=a+b+c+d;
+		    if(sum>=1){
+		        System.out.println("out");
+		    }
+		    else{
+		        System.out.println("in");
+		    }
+		}
+
+	}
+}

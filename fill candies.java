de43da70c61f1e2b2889d06ee76d069bb5249ec2
Shruti <shruti.developer18@gmail.com>
2026-09-08 18:22:44 +0530

    Create fill candies.java

---
+++ fill candies.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for(int i=0;i<t;i++){
+		    int n=s.nextInt();
+		    int k=s.nextInt();
+		    int m=s.nextInt();
+		    if(n%(m*k)==0){
+		        System.out.println(n/(m*k));
+		    }
+		    else{
+		        System.out.println((n/(m*k))+1);
+		    }
+		}
+		
+
+	}
+}
